da4737eef1702395ecdce6b7503800215ec7a9e3
beingj <beingj@gmail.com>
2019-08-12 16:42:24 +0800

    126

--- 0126-WordLadderII.cs
+++ 0126-WordLadderII.cs
@@ -7,18 +7,120 @@ using System.Text;
 
 namespace WordLadderII
 {
+    public class Solution1
+    {
+        public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
+        {
+            if (!wordList.Contains(endWord))
+                return new List<IList<string>>();
+            // if (beginWord.Length == 1)
+            // {
+            //     var res = new List<IList<string>>();
+            //     res.Add(new List<string> { beginWord, endWord });
+            //     return res;
+            // }
+            var wordSet = new HashSet<string>(wordList.Where(i => i != beginWord));
+            var cur_level = new List<string> { beginWord };
+            var next_level = new List<string>();
+            var depth = 1;
+            var n = beginWord.Length;
+            var parentDict = new Dictionary<string, List<string>> { { beginWord, new List<string> { null } } };
+            var minDepth = wordList.Count + 1;
+            var leafList = new List<string>();
+            while (cur_level.Count > 0)
+            {
+                foreach (var item in cur_level)
+                {
+                    for (var i = 0; i < n; i++)
+                    {
+                        foreach (var c in "abcdefghijklmnopqrstuvwxyz")
+                        {
+                            var word = item.Substring(0, i) + c + item.Substring(i + 1);
+                            if (wordSet.Contains(word))
+                            {
+                                if (word == endWord)
+                                {
+                                    leafList.Add(item);
+                                }
+                                else
+                                {
+                                    if (!parentDict.ContainsKey(word))
+                                        parentDict[word] = new List<string>();
+                                    parentDict[word].Add(item);
+                                    next_level.Add(word);
+                                }
+                            }
+                        }
+                    }
+                }
+                if (leafList.Count > 0)
+                    break;
+                foreach (var word in next_level)
+                    wordSet.Remove(word);
+
+                depth += 1;
+                cur_level.Clear();
+                foreach (var i in next_level)
+                {
+                    cur_level.Add(i);
+                }
+                next_level.Clear();
+                // cur_level = next_level;
+                // next_level = new List<string>();
+            }
+            var pathList = new List<IList<string>>();
+
+            void GetPath(string node, List<string> path)
+            {
+                // TODO: very slow, cause time limit exceed
+                path.Add(node);
+                foreach (var p in parentDict[node])
+                {
+                    var path2 = new List<string>();
+                    path2.AddRange(path);
+                    if (p == null)
+                    {
+                        path2.Reverse();
+                        path2.Add(endWord);
+                        pathList.Add(path2);
+                    }
+                    else
+                    {
+                        GetPath(p, path2);
+                    }
+                }
+            }
+            foreach (var leaf in leafList)
+            {
+                var path = new List<string>();
+                GetPath(leaf, path);
+            }
+            if (pathList.Count == 0)
+                return new List<IList<string>>();
+            var minLen = pathList.Select(i => i.Count).Min();
+            var x = pathList.Where(i => i.Count == minLen).Select(i => string.Join(',', i)).Distinct();
+            var y = new List<IList<string>>();
+            foreach (var i in x)
+            {
+                y.Add(i.Split(',').ToList());
+            }
+            return y;
+        }
+    }
     public class Solution
     {
         public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
         {
+            // give up.
+            // copy from: https://cloud.tencent.com/developer/article/1407026
             var front_level = new HashSet<string> { beginWord };
             var end_level = new HashSet<string> { endWord };
             var path_dic = new Dictionary<string, List<string>>();
             var wordSet = new HashSet<string>(wordList);
-            if (wordSet.Contains(endWord))
+            if (!wordSet.Contains(endWord))
                 return new List<IList<string>>();
             bfs(front_level, end_level, true, wordSet, path_dic);
-            var path = new List<string>();
+            var path = new List<string> { beginWord };
             var paths = new List<IList<string>>();
             construct_path(beginWord, endWord, path_dic, path, paths);
             return paths;
@@ -88,102 +190,6 @@ namespace WordLadderII
                 }
             }
         }
-        // public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList)
-        // {
-        //     // if (!wordList.Contains(endWord))
-        //     //     return new List<IList<string>>();
-        //     // if (beginWord.Length == 1)
-        //     // {
-        //     //     var res = new List<IList<string>>();
-        //     //     res.Add(new List<string> { beginWord, endWord });
-        //     //     return res;
-        //     // }
-        //     var wordSet = new HashSet<string>(wordList.Where(i => i != beginWord));
-        //     var cur_level = new List<string> { beginWord };
-        //     var next_level = new List<string>();
-        //     var depth = 1;
-        //     var n = beginWord.Length;
-        //     var parentDict = new Dictionary<string, List<string>> { { beginWord, new List<string> { null } } };
-        //     var minDepth = wordList.Count + 1;
-        //     var leafList = new List<string>();
-        //     while (cur_level.Count > 0)
-        //     {
-        //         foreach (var item in cur_level)
-        //         {
-        //             for (var i = 0; i < n; i++)
-        //             {
-        //                 foreach (var c in "abcdefghijklmnopqrstuvwxyz")
-        //                 {
-        //                     var word = item.Substring(0, i) + c + item.Substring(i + 1);
-        //                     if (wordSet.Contains(word))
-        //                     {
-        //                         if (word == endWord)
-        //                         {
-        //                             leafList.Add(item);
-        //                         }
-        //                         else
-        //                         {
-        //                             if (!parentDict.ContainsKey(word))
-        //                                 parentDict[word] = new List<string>();
-        //                             parentDict[word].Add(item);
-        //                             next_level.Add(word);
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //         if (leafList.Count > 0)
-        //             break;
-        //         foreach (var word in next_level)
-        //             wordSet.Remove(word);
-
-        //         depth += 1;
-        //         cur_level.Clear();
-        //         foreach (var i in next_level)
-        //         {
-        //             cur_level.Add(i);
-        //         }
-        //         next_level.Clear();
-        //         // cur_level = next_level;
-        //         // next_level = new List<string>();
-        //     }
-        //     var pathList = new List<IList<string>>();
-
-        //     void GetPath(string node, List<string> path)
-        //     {
-        //         path.Add(node);
-        //         foreach (var p in parentDict[node])
-        //         {
-        //             var path2 = new List<string>();
-        //             path2.AddRange(path);
-        //             if (p == null)
-        //             {
-        //                 path2.Reverse();
-        //                 path2.Add(endWord);
-        //                 pathList.Add(path2);
-        //             }
-        //             else
-        //             {
-        //                 GetPath(p, path2);
-        //             }
-        //         }
-        //     }
-        //     foreach (var leaf in leafList)
-        //     {
-        //         var path = new List<string>();
-        //         GetPath(leaf, path);
-        //     }
-        //     if (pathList.Count == 0)
-        //         return new List<IList<string>>();
-        //     var minLen = pathList.Select(i => i.Count).Min();
-        //     var x = pathList.Where(i => i.Count == minLen).Select(i => string.Join(',', i)).Distinct();
-        //     var y = new List<IList<string>>();
-        //     foreach (var i in x)
-        //     {
-        //         y.Add(i.Split(',').ToList());
-        //     }
-        //     return y;
-        // }
     }
     public class Test
     {
